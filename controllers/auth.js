50d73b309ce35bf818c23ad622ba6c0c675ccbd1
samirjkhadka <findsamirkhadka@gmail.com>
2024-10-06 15:12:31 +0545

    invalid response updated

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,15 +19,21 @@ exports.register = async function (req, res) {
     user = await user.save();
 
     if (!user) {
-      return res
-        .status(500)
-        .json({
-          type: "Internal Server Error",
-          message: "Something went wrong, Couldn't create a new user",
-        });
+      return res.status(500).json({
+        type: "Internal Server Error",
+        message: "Something went wrong, Couldn't create a new user",
+      });
     }
     return res.status(201).json({ user });
   } catch (error) {
+    if (error.message.includes("email_1 dup key")) {
+      return res
+        .status(409)
+        .json({
+          type: "AuthError",
+          message: "User with the email already exists",
+        });
+    }
     return res.status(500).json({ type: error.name, message: error.message });
   }
 };
